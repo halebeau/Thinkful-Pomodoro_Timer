13fd0c1272b50e9ed4f8b375b5d369f2c6fb6a80
Beau Hale <halebeau3@gmail.com>
2021-09-20 15:47:02 -0700

    Added: frontend deployment - vercel & heroku

---
+++ build/precache-manifest.bab8b7a4c027298d6774315f45591b66.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "adb7c4340290ceafd7c4aea7e9a7901e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cd3c76aa22593caf05a0",
+    "url": "/static/css/main.cb9e985a.chunk.css"
+  },
+  {
+    "revision": "8f60e831c49dd590640b",
+    "url": "/static/js/2.0acf020f.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.0acf020f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd3c76aa22593caf05a0",
+    "url": "/static/js/main.fdf94b9e.chunk.js"
+  },
+  {
+    "revision": "facd31b36309f08ff9ad",
+    "url": "/static/js/runtime-main.acde468f.js"
+  }
+]);
